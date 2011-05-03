cf5bab3b5afa0aece139f36320ad255a52c3e9fe
Yves Bonjour <yves.bonjour@gmail.com>
2011-05-03 18:54:27 +0200

    added isStatusOk tests, RouteWebRequesterTest

--- src/ch/tripplanner/test/routes/JSONRouteParserTest.java
+++ src/ch/tripplanner/test/routes/JSONRouteParserTest.java
@@ -56,6 +56,52 @@ public class JSONRouteParserTest extends TestCase {
 		assertEquals(duration, route.getDuration());
 	}
 	
+	public void test_isStatusOk(){
+		//Arrange
+		Location start = new LocationMock(46.57, 7.28);
+		Location destination = new LocationMock(47.35, 7.35);
+		RouteType type = RouteType.CAR;
+		int duration = 5000;
+		String input = createJSONRouteString(start, destination, type, duration);
+		
+		//Act
+		JSONRouteParser parser = new JSONRouteParser(input);
+		boolean statusOk = parser.isStatusOk();
+		
+		//Assert
+		assertTrue(statusOk);
+	}
+	
+	public void test_isStatusOk_with_error(){
+		//Arrange
+		String input = createJSONErrorString();
+		
+		//Act
+		JSONRouteParser parser = new JSONRouteParser(input);
+		boolean statusOk = parser.isStatusOk();
+		
+		//Assert
+		assertFalse(statusOk);
+	}
+	
+	public void test_isStatusOk_with_no_status(){
+		//Arrange
+		String input = "{\"foo\":\"true\"}";
+		
+		//Act
+		JSONRouteParser parser = new JSONRouteParser(input);
+		try{
+			parser.isStatusOk();
+			fail();
+		}catch(RuntimeException e){
+			//Test succeeded
+		}
+		
+	}
+	
+	private String createJSONErrorString(){
+		return "{\"status\":\"ERROR\",\"message\":\"Some error description\"}";
+	}
 	
 	private String createJSONRouteString(Location start, Location destination, RouteType type, int duration){
 		return "{\"status\":\"OK\",\"route\":{\"startLongitude\":\"" + start.getLongitude() + "\","
--- src/ch/tripplanner/test/routes/RouteWebRequesterTest.java
+++ src/ch/tripplanner/test/routes/RouteWebRequesterTest.java
@@ -1,20 +1,32 @@
 package ch.tripplanner.test.routes;
 
-import junit.framework.Assert;
+import junit.framework.TestCase;
 import android.location.Location;
 import ch.tripplanner.routes.Route;
 import ch.tripplanner.routes.RouteRequest;
+import ch.tripplanner.routes.RouteType;
 import ch.tripplanner.routes.RouteWebRequester;
 
-public class RouteWebRequesterTest {
+public class RouteWebRequesterTest extends TestCase{
 
-	public void testWebRequest(){		
+	public void test_WebRequest(){
+		//Arrange
 		Location start = new LocationMock(46.57, 7.28);
 		Location destination = new LocationMock(47.35, 7.35);
-		/*
-		RouteRequest request = new RouteRequest(start, destination, Route.Type.PUBLIC_TRANSPORTATION);
+		RouteType type = RouteType.PUBLIC_TRANSPORTATION;
+		RouteRequest request = new RouteRequest(start, destination, type);
+		
+		//Act
 		RouteWebRequester requester = new RouteWebRequester(request);
 		Route route = requester.executeWebRequest();
-		Assert.assertNotNull(route);*/
+		
+		//Assert
+		assertNotNull(route);
+		assertEquals(start.getLongitude(), route.getStart().getLongitude());
+		assertEquals(start.getLatitude(), route.getStart().getLatitude());
+		assertEquals(destination.getLongitude(), route.getDestination().getLongitude());
+		assertEquals(destination.getLatitude(), route.getDestination().getLatitude());
+		assertEquals(route.getType(), type);
+		assertTrue(route.getDuration() > 0);
 	}
 }
